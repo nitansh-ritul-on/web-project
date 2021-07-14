73300b1145f83ecf53486296e68f75197d75223f
nitansh-ritul-on <57222566+nitansh-ritul-on@users.noreply.github.com>
2021-07-14 20:44:13 +0530

    opps => oops

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.get("/weather", (req, res) => {
 });
 app.get("*", (req, res) => {
     res.render("404error", {
-      errMsg: 'Opps! Page Not Found'
+      errMsg: 'Oops! Page Not Found'
   });
 });
 app.listen(port, () => {
