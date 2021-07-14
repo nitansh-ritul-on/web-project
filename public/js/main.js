1b54c29806c8aa2ab4277ec8a78bfd2d3e6533d7
nitansh-ritul-on <57222566+nitansh-ritul-on@users.noreply.github.com>
2021-07-14 18:22:10 +0530

    nitu temperature app

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+// const log = console.log;
+// const submitBtn = document.getElementById("submit-btn");
+
+// const getInfo = (event) => {
+//     event.preventDefault();
+//     alert("hi");
+// };
+
+// submitBtn.addEventListener("click", getInfo);
---
+++ src/app.js
@@ -0,0 +1,33 @@
+const log = console.log;
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const app = express();
+const port = 8000;
+
+const staticPath = path.join(__dirname, "../public");
+const templatePath = path.join(__dirname, "../src/templates/views");
+const partialsPath = path.join(__dirname, "../src/templates/partials");
+app.set("view engine", "hbs");
+app.set('views', templatePath)
+hbs.registerPartials(partialsPath)
+
+
+app.use(express.static(staticPath));
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/weather", (req, res) => {
+  res.render("weather");
+});
+app.get("*", (req, res) => {
+    res.render("404error", {
+      errMsg: 'Opps! Page Not Found'
+  });
+});
+app.listen(port, () => {
+  console.log(`listening to the port at ${port}`);
+});
